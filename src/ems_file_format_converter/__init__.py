d3f4d624d7e9121b3da899e60f2cbdcf75716c25
Eskandari <eskandarih@ssil.co.jp>
2026-05-19 14:00:07 +0900

    refactor: simplify GitHub Actions workflow by removing release tag validation and dynamic versioning

--- src/ems_file_format_converter/__init__.py
+++ src/ems_file_format_converter/__init__.py
@@ -2,4 +2,6 @@
 # Copyright (c) 2025 Science Solutions International Laboratory, Inc.
 from . import atlas, femap, unv
 
-__all__ = ["atlas", "femap", "unv"]
+__version__ = "0.5.1"
+
+__all__ = ["atlas", "femap", "unv", "__version__"]
